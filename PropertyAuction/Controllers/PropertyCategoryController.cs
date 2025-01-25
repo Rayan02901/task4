24a3ef7f49da5f600f1ec440b57b625c46fba5cc
“[Rayan <[rayan077@academiacollege.edu.np]>
2025-01-25 21:13:57 +0545

    PropertyCategoryAdded

---
+++ PropertyAuction/Controllers/PropertyCategoryController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PropertyAuction.Data;
+using PropertyAuction.Models;
+
+namespace PropertyAuction.Controllers
+{
+    public class PropertyCategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public PropertyCategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<PropertyCategory> objCategoryList = _db.PropertyCategories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(PropertyCategory obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.PropertyCategories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            PropertyCategory? propertycategoryFromDb = _db.PropertyCategories.Find(id);
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            //Category? categoryFromDb2 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
+
+            if (propertycategoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(propertycategoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(PropertyCategory obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.PropertyCategories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            PropertyCategory? propertycategoryFromDb = _db.PropertyCategories.Find(id);
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            //Category? categoryFromDb2 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
+            if (propertycategoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(propertycategoryFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            PropertyCategory? obj = _db.PropertyCategories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.PropertyCategories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- PropertyAuction/obj/Debug/net8.0/PropertyAuction.AssemblyInfo.cs
+++ PropertyAuction/obj/Debug/net8.0/PropertyAuction.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PropertyAuction")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fc59505c77e64d33465d8e738a423c874bdf3c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("PropertyAuction")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PropertyAuction")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
